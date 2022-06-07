bd6b889cee98c6d92286d9448b3b6930acde012d
nehabhange <neha05bhange@gmail.com>
2022-06-07 09:03:53 +0530

    commit

--- data.js
+++ data.js
@@ -133,28 +133,28 @@ export const data = [{
     },
 ];
 
-export const users = [{
-        id: 1,
-        logo: "yahoo.png",
-        avatar: "user1.jpeg",
-        comment: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Utconsectetur veniam fugiat labore quaerat possimus laudantium.",
-        name: "Harold Brown",
-        title: "Executive Manager",
-    },
-    {
-        id: 2,
-        logo: "google.png",
-        avatar: "user2.jpeg",
-        comment: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Utconsectetur veniam fugiat labore quaerat possimus laudantium.",
-        name: "Anna Taylor",
-        title: "HR Manager",
-    },
-    {
-        id: 3,
-        logo: "facebook.png",
-        avatar: "user3.jpeg",
-        comment: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Utconsectetur veniam fugiat labore quaerat possimus laudantium.",
-        name: "James Garry",
-        title: "UI Designer",
-    },
-];
+// export const users = [{
+//         id: 1,
+//         logo: "yahoo.png",
+//         avatar: "user1.jpeg",
+//         comment: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Utconsectetur veniam fugiat labore quaerat possimus laudantium.",
+//         name: "Harold Brown",
+//         title: "Executive Manager",
+//     },
+//     {
+//         id: 2,
+//         logo: "google.png",
+//         avatar: "user2.jpeg",
+//         comment: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Utconsectetur veniam fugiat labore quaerat possimus laudantium.",
+//         name: "Anna Taylor",
+//         title: "HR Manager",
+//     },
+//     {
+//         id: 3,
+//         logo: "facebook.png",
+//         avatar: "user3.jpeg",
+//         comment: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Utconsectetur veniam fugiat labore quaerat possimus laudantium.",
+//         name: "James Garry",
+//         title: "UI Designer",
+//     },
+// ];
